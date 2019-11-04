ef34094e1f902a4924b6fb5803ceac0e3739a2f3
martin atkinson <martin.atkinson@gmail.com>
2019-11-04 12:19:47 +1100

    added home page test

--- test/test-server.js
+++ test/test-server.js
@@ -5,3 +5,24 @@ author: martin atkinson
 date:   4 November 2019
 
 */
+
+var expect  = require('chai').expect;
+var request = require('request');
+
+var home_url = "http://localhost:8080/";
+
+describe('Home page', function() {
+    it('status', function(done){
+        request(home_url, function(error, response, body) {
+            expect(response.statusCode).to.equal(200);
+            done();
+        });
+    });
+
+    it('content', function(done) {
+        request(home_url , function(error, response, body) {
+            expect(body).to.equal('Welcome to the flights app.');
+            done();
+        });
+    });
+});
